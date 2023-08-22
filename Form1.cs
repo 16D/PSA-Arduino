19f986d35c8e4935a47b185e168c9cd4d3e63885
Łukasz <lsobon@gmail.com>
2023-08-22 23:22:03 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -26,7 +26,7 @@ namespace PSA_CVM2
         public string ARTIV = ">6B6:696";
         public string TELEMAT = ">764:664";
         public string COMBINE = ">75F:65F";
-        public string resultCodingCVM;
+        private string resultCodingCVM;
 
         public Form1()
         {
